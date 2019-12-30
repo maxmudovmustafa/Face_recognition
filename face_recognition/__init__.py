22498016b6b2ea7778b7ef8f63263c39610bf4f9
MrShoxruxbek <shohruh.maxmudov@mail.ru>
2019-12-30 17:34:33 +0500

    refactoring

--- face_recognition/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = """Shoxruxbek Maxmudov"""
-__email__ = 'shohruh.maxmudov@mail.ru'
-__version__ = '1.2.3'
-
-from .api import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
